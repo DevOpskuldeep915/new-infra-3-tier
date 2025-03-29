b1c71b3684ad1c37b859fd15e69904c3026e67fd
Kuldeep Mohanty <devops.kuldeep915@gmail.com>
2025-03-29 13:23:17 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
-# Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "swiggy-kbjdfgdkfhkxgxncfhxdbkfh"
-
-  # Enable versioning
-  versioning {
-    enabled = true
-  }
 }
 
+resource "aws_s3_bucket_versioning" "example_versioning" {
+  bucket = aws_s3_bucket.example_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
